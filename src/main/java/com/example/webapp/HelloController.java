8b98b5319c7e3832f73dd72b263afc8addbdf834
finite-la-kirilledia <kirill040994@gmail.com>
2023-09-22 21:45:56 +0200

    add HelloController

---
+++ src/main/java/com/example/webapp/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.webapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello";
+    }
+}
